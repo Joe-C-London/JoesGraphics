a0440ffc0cfe72a0dc4654b9bba09eb8922e67dd
Joe-C-London <joe.collins.se@gmail.com>
2020-02-22 23:17:12 +0000

    Listing frame template

---
+++ src/main/java/com/joecollins/graphics/components/ListingFrameBuilder.java
@@ -0,0 +1,54 @@
+package com.joecollins.graphics.components;
+
+import com.joecollins.bindings.BindableList;
+import com.joecollins.bindings.Binding;
+import com.joecollins.bindings.IndexedBinding;
+import java.awt.Color;
+import java.util.List;
+import java.util.function.Function;
+
+public class ListingFrameBuilder {
+
+  private BarFrame barFrame = new BarFrame();
+
+  public static <T> ListingFrameBuilder of(
+      BindableList<T> list,
+      Function<T, String> leftTextFunc,
+      Function<T, String> rightTextFunc,
+      Function<T, Color> colorFunc) {
+    ListingFrameBuilder builder = new ListingFrameBuilder();
+    BarFrame barFrame = builder.barFrame;
+    barFrame.setNumBarsBinding(Binding.sizeBinding(list));
+    barFrame.setLeftTextBinding(IndexedBinding.propertyBinding(list, leftTextFunc));
+    barFrame.setRightTextBinding(IndexedBinding.propertyBinding(list, rightTextFunc));
+    barFrame.addSeriesBinding(
+        "Item",
+        IndexedBinding.propertyBinding(list, colorFunc),
+        IndexedBinding.propertyBinding(list, x -> 1));
+    return builder;
+  }
+
+  public static <T> ListingFrameBuilder of(
+      Binding<List<T>> list,
+      Function<T, String> leftTextFunc,
+      Function<T, String> rightTextFunc,
+      Function<T, Color> colorFunc) {
+    BindableList<T> bindableList = new BindableList<>();
+    list.bind(bindableList::setAll);
+    return of(bindableList, leftTextFunc, rightTextFunc, colorFunc);
+  }
+
+  public ListingFrameBuilder withHeader(Binding<String> headerBinding) {
+    barFrame.setHeaderBinding(headerBinding);
+    return this;
+  }
+
+  public ListingFrameBuilder withSubhead(Binding<String> subheadBinding) {
+    barFrame.setSubheadTextBinding(subheadBinding);
+    return this;
+  }
+
+  public BarFrame build() {
+    return barFrame;
+  }
+}
