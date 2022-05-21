0b56866648aaf92b1474fdd320b386b22f4b8320
Joe-C-London <joe.collins.se@gmail.com>
2022-05-21 18:41:54 +0100

    Darken Others party to improve contrast on maps

--- src/main/kotlin/com/joecollins/models/general/Party.kt
+++ src/main/kotlin/com/joecollins/models/general/Party.kt
@@ -6,6 +6,6 @@ data class Party(val name: String, val abbreviation: String, val color: Color) {
     override fun toString(): String = abbreviation
 
     companion object {
-        @JvmField val OTHERS = Party("Others", "OTH", Color.GRAY)
+        @JvmField val OTHERS = Party("Others", "OTH", Color.DARK_GRAY)
     }
 }
