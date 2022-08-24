a4c241381a652702a088ca242ca8444fa68aa245
Joe-C-London <joe.collins.se@gmail.com>
2022-08-24 22:29:09 +0100

    Swingometer screen bug fix: calculating swing needed to win with uncontested seats

--- src/main/kotlin/com/joecollins/graphics/screens/generic/SwingometerScreen.kt
+++ src/main/kotlin/com/joecollins/graphics/screens/generic/SwingometerScreen.kt
@@ -416,7 +416,11 @@ class SwingometerScreen private constructor(title: Flow.Publisher<out String?>, 
                     val total = m.value.values.sum()
                     val focus = m.value[focusParty] ?: 0
                     val comp = m.value[compParty] ?: 0
-                    val position = 0.5 * (comp - focus) / total
+                    val position: Double
+                    if (total == 0)
+                        position = if (m.value.containsKey(focusParty)) Double.NEGATIVE_INFINITY else Double.POSITIVE_INFINITY
+                    else
+                        position = 0.5 * (comp - focus) / total
                     val count = inputs.weights?.let { f -> f(m.key) } ?: 1
                     generateSequence { position }.take(count)
                 }
